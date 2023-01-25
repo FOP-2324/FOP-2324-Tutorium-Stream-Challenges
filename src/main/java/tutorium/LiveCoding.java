6115ce0853ce489e7a62295229f3d9e5b786ddad
Rdeisenroth <rdeisenroth@gmail.com>
2023-01-25 14:19:02 +0100

    LiveCoding file

---
+++ src/main/java/tutorium/LiveCoding.java
@@ -0,0 +1,17 @@
+package tutorium;
+
+/**
+ * This class is used for live coding during the tutorium.
+ */
+public class LiveCoding {
+
+        /**
+        * Executes the live coding.
+        *
+        * @param args the command line arguments
+        */
+        public static void main(String[] args) {
+            System.out.println("Hello World!");
+        }
+
+}
