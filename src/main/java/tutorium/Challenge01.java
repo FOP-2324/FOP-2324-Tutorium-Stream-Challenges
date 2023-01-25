ae78a469f9e932c2a155e5c56dcce6b74d92d1e0
Rdeisenroth <rdeisenroth@gmail.com>
2023-01-25 13:14:11 +0100

    first challenge

--- src/main/java/tutorium/Challenge01.java
+++ src/main/java/tutorium/Challenge01.java
@@ -2,13 +2,24 @@ package tutorium;
 
 import java.util.List;
 
+/**
+ * The first Stream challenge of the tutorium.
+ *
+ * @author Ruben Deisenroth
+ */
 public class Challenge01 {
 
+    /**
+     * Executes the challenge.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
 
-        List<String> list = List.of("Uga","Buga","Suga","Nope");
+        List<String> list = List.of("Uga", "Buga", "Suga", "Nope");
 
-        // Filter Nope aus der Liste heraus und speichere es in result
+        // Filter alle Strings aus der Liste, die mit "a" aufhören
+        // Hinweis: Methode endsWith() von String
         List<String> result = null;
         // <USER CODE>
 
